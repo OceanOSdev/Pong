ce198e1e717b18c7b70540fbe3de9fdd818092fc
Thomas Maloney <thomastnf@live.com>
2015-08-30 17:52:44 -0500

    Added VB Pong Project

---
+++ PongVB/MainForm.Designer.vb
@@ -0,0 +1,343 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrClock = New System.Windows.Forms.Timer(Me.components)
+        Me.paddlePlayer = New System.Windows.Forms.PictureBox()
+        Me.picBall = New System.Windows.Forms.PictureBox()
+        Me.paddleComputer = New System.Windows.Forms.PictureBox()
+        Me.lblComputerScore = New System.Windows.Forms.Label()
+        Me.lblPlayerScore = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox15 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox16 = New System.Windows.Forms.PictureBox()
+        CType(Me.paddlePlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.paddleComputer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrClock
+        '
+        Me.tmrClock.Enabled = True
+        Me.tmrClock.Interval = 1
+        '
+        'paddlePlayer
+        '
+        Me.paddlePlayer.BackColor = System.Drawing.Color.White
+        Me.paddlePlayer.Location = New System.Drawing.Point(42, 223)
+        Me.paddlePlayer.Name = "paddlePlayer"
+        Me.paddlePlayer.Size = New System.Drawing.Size(16, 128)
+        Me.paddlePlayer.TabIndex = 0
+        Me.paddlePlayer.TabStop = False
+        '
+        'picBall
+        '
+        Me.picBall.BackColor = System.Drawing.Color.White
+        Me.picBall.Location = New System.Drawing.Point(64, 267)
+        Me.picBall.Name = "picBall"
+        Me.picBall.Size = New System.Drawing.Size(20, 20)
+        Me.picBall.TabIndex = 1
+        Me.picBall.TabStop = False
+        '
+        'paddleComputer
+        '
+        Me.paddleComputer.BackColor = System.Drawing.Color.White
+        Me.paddleComputer.Location = New System.Drawing.Point(1157, 223)
+        Me.paddleComputer.Name = "paddleComputer"
+        Me.paddleComputer.Size = New System.Drawing.Size(16, 128)
+        Me.paddleComputer.TabIndex = 2
+        Me.paddleComputer.TabStop = False
+        '
+        'lblComputerScore
+        '
+        Me.lblComputerScore.BackColor = System.Drawing.Color.Transparent
+        Me.lblComputerScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblComputerScore.ForeColor = System.Drawing.Color.White
+        Me.lblComputerScore.Location = New System.Drawing.Point(623, 19)
+        Me.lblComputerScore.Name = "lblComputerScore"
+        Me.lblComputerScore.Size = New System.Drawing.Size(119, 37)
+        Me.lblComputerScore.TabIndex = 3
+        Me.lblComputerScore.Text = "0"
+        Me.lblComputerScore.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lblPlayerScore
+        '
+        Me.lblPlayerScore.BackColor = System.Drawing.Color.Transparent
+        Me.lblPlayerScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlayerScore.ForeColor = System.Drawing.Color.White
+        Me.lblPlayerScore.Location = New System.Drawing.Point(472, 19)
+        Me.lblPlayerScore.Name = "lblPlayerScore"
+        Me.lblPlayerScore.Size = New System.Drawing.Size(119, 37)
+        Me.lblPlayerScore.TabIndex = 4
+        Me.lblPlayerScore.Text = "0"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(597, 267)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.White
+        Me.PictureBox2.Location = New System.Drawing.Point(597, 234)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.White
+        Me.PictureBox3.Location = New System.Drawing.Point(597, 168)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.White
+        Me.PictureBox4.Location = New System.Drawing.Point(597, 201)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.White
+        Me.PictureBox5.Location = New System.Drawing.Point(597, 36)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox5.TabIndex = 12
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.White
+        Me.PictureBox6.Location = New System.Drawing.Point(597, 69)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox6.TabIndex = 11
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.White
+        Me.PictureBox7.Location = New System.Drawing.Point(597, 102)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox7.TabIndex = 10
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.White
+        Me.PictureBox8.Location = New System.Drawing.Point(597, 135)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox8.TabIndex = 9
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.White
+        Me.PictureBox9.Location = New System.Drawing.Point(597, 300)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox9.TabIndex = 20
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.BackColor = System.Drawing.Color.White
+        Me.PictureBox10.Location = New System.Drawing.Point(597, 333)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox10.TabIndex = 19
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.BackColor = System.Drawing.Color.White
+        Me.PictureBox11.Location = New System.Drawing.Point(597, 366)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox11.TabIndex = 18
+        Me.PictureBox11.TabStop = False
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.Color.White
+        Me.PictureBox12.Location = New System.Drawing.Point(597, 399)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox12.TabIndex = 17
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBox13
+        '
+        Me.PictureBox13.BackColor = System.Drawing.Color.White
+        Me.PictureBox13.Location = New System.Drawing.Point(597, 432)
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox13.TabIndex = 16
+        Me.PictureBox13.TabStop = False
+        '
+        'PictureBox14
+        '
+        Me.PictureBox14.BackColor = System.Drawing.Color.White
+        Me.PictureBox14.Location = New System.Drawing.Point(597, 465)
+        Me.PictureBox14.Name = "PictureBox14"
+        Me.PictureBox14.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox14.TabIndex = 15
+        Me.PictureBox14.TabStop = False
+        '
+        'PictureBox15
+        '
+        Me.PictureBox15.BackColor = System.Drawing.Color.White
+        Me.PictureBox15.Location = New System.Drawing.Point(597, 498)
+        Me.PictureBox15.Name = "PictureBox15"
+        Me.PictureBox15.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox15.TabIndex = 14
+        Me.PictureBox15.TabStop = False
+        '
+        'PictureBox16
+        '
+        Me.PictureBox16.BackColor = System.Drawing.Color.White
+        Me.PictureBox16.Location = New System.Drawing.Point(597, 531)
+        Me.PictureBox16.Name = "PictureBox16"
+        Me.PictureBox16.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox16.TabIndex = 13
+        Me.PictureBox16.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1215, 584)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox13)
+        Me.Controls.Add(Me.PictureBox14)
+        Me.Controls.Add(Me.PictureBox15)
+        Me.Controls.Add(Me.PictureBox16)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblPlayerScore)
+        Me.Controls.Add(Me.lblComputerScore)
+        Me.Controls.Add(Me.paddleComputer)
+        Me.Controls.Add(Me.picBall)
+        Me.Controls.Add(Me.paddlePlayer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmMain"
+        Me.Text = "Pong VB"
+        CType(Me.paddlePlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.paddleComputer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tmrClock As Timer
+    Friend WithEvents paddlePlayer As PictureBox
+    Friend WithEvents picBall As PictureBox
+    Friend WithEvents paddleComputer As PictureBox
+    Friend WithEvents lblComputerScore As Label
+    Friend WithEvents lblPlayerScore As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents PictureBox14 As PictureBox
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents PictureBox16 As PictureBox
+End Class
---
+++ PongVB/MainForm.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class frmMain
+#Region "globals"
+    Dim speed As Single = 10 ' Ball Speed  
+    Dim rndInst As New Random() ' Random instance  
+    Dim xVel As Single = Math.Cos(rndInst.Next(5, 10)) * speed  ' why the hell is a "float" called a single?
+    Dim yVel As Single = Math.Sin(rndInst.Next(5, 10)) * speed
+    ' The player's scores.
+    Dim compScore As Integer = 0
+    Dim plrScore As Integer = 0
+    ' Multiplayer?
+    Dim blnMult As Boolean = False
+#End Region
+
+    ''' <summary>
+    ''' Gets state of Key Asynchronously (what ever that is lol)
+    ''' </summary>
+    ''' <param name="vKey">The Key to check the state of</param>
+    ''' <returns>Hell if I know</returns>
+    ''' <remarks>Pretty dank</remarks>
+    <DllImport("user32.dll")>
+    Public Shared Function GetAsyncKeyState(ByVal vKey As Int32) As UShort
+        ' This function is empty and it still does stuff lol, how dank is that?
+    End Function
+
+    ''' <summary>
+    ''' Sets location of picture box
+    ''' </summary>
+    ''' <param name="location">The new location of the picture box</param>
+    ''' <remarks>nothing revolutionary</remarks>
+    Private Sub SetLocation(ByVal [location] As Point)
+        Me.paddlePlayer.Location = [location]
+    End Sub
+
+    ''' <summary>
+    ''' Technically just an event handler for the timer, however I'm using it as the game loop.
+    ''' </summary>
+    ''' <param name="sender">
+    ''' used to distinguish between senders, for example, if I had this 
+    ''' handle both tmrClock and another timer, I'd use sender to see
+    ''' which event is being handled
+    ''' </param>
+    ''' <param name="e">Event arguments</param>
+    ''' <remarks>kinda hacky, but meh</remarks>
+    Private Sub tmrClock_Tick(sender As Object, e As EventArgs) Handles tmrClock.Tick
+#Region "Player Movement"
+        REM moves the player's paddle up 8 points
+        If (GetAsyncKeyState(Keys.W)) Then
+            Dim newLoc As New Point(paddlePlayer.Location.X, paddlePlayer.Location.Y - 8)
+            SetLocation(newLoc)
+        End If
+
+        REM moves the player's paddle 8 points
+        If (GetAsyncKeyState(Keys.S)) Then
+            Dim newLoc As New Point(paddlePlayer.Location.X, paddlePlayer.Location.Y + 8)
+            SetLocation(newLoc)
+        End If
+#End Region
+
+#Region "Computer Player Movement"
+        If Not blnMult Then
+            If picBall.Location.Y > 5 And picBall.Location.Y < Me.Height - 40 - paddlePlayer.Height Then
+                paddleComputer.Location = New Point(paddleComputer.Location.X, picBall.Location.Y)
+            End If
+        Else
+            If GetAsyncKeyState(Keys.Up) Or GetAsyncKeyState(Keys.I) Then
+                paddleComputer.Location = New Point(paddleComputer.Location.X, paddleComputer.Location.Y - 8)
+            End If
+
+            If GetAsyncKeyState(Keys.Down) Or GetAsyncKeyState(Keys.K) Then
+                paddleComputer.Location = New Point(paddleComputer.Location.X, paddleComputer.Location.Y + 8)
+            End If
+        End If
+#End Region
+
+#Region "Multiplayer"
+        If (GetAsyncKeyState(Keys.P)) Then
+            blnMult = Not blnMult
+        End If
+#End Region
+
+#Region "Ball Movement"
+        ' Move the game ball.  
+        picBall.Location = New Point(picBall.Location.X + xVel, picBall.Location.Y + yVel)
+#End Region
+
+#Region "Collision"
+        ' Check for top wall.
+        If picBall.Location.Y < 0 Then
+            picBall.Location = New Point(picBall.Location.X, 0)
+            yVel = -yVel
+        End If
+
+        ' Check for bottom wall.
+        If picBall.Location.Y > Me.Height - picBall.Size.Height - 45 Then
+            picBall.Location = New Point(picBall.Location.X, Me.Height - picBall.Size.Height - 45)
+            yVel = -yVel
+        End If
+
+        ' Check for computer paddle.
+        If picBall.Bounds.IntersectsWith(paddleComputer.Bounds) Then
+            picBall.Location = New Point(paddleComputer.Location.X - paddleComputer.Size.Width - 1,
+            picBall.Location.Y)
+            xVel = -xVel
+        End If
+
+        ' Check for player paddle.
+        If picBall.Bounds.IntersectsWith(paddlePlayer.Bounds) Then
+            picBall.Location = New Point(paddlePlayer.Location.X + paddlePlayer.Size.Width + 1,
+            picBall.Location.Y)
+            xVel = -xVel
+        End If
+
+        ' Check for right wall.
+        If picBall.Location.X > Me.Width - picBall.Size.Width - paddlePlayer.Width Then
+            plrScore += 1
+            picBall.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+            lblPlayerScore.Text = Convert.ToString(plrScore)
+        End If
+
+        ' Check for left wall.
+        If picBall.Location.X < 0 Then
+            compScore += 1
+            picBall.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+            lblComputerScore.Text = Convert.ToString(compScore)
+        End If
+#End Region
+    End Sub
+End Class
